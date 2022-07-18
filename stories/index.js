625dc6b6238e0510d1f5994620febae756ed2351
siopao2001 <sio_pao2001@yahoo.ca>
2022-07-17 21:29:33 -0400

    create Confirm viewand story

--- stories/index.js
+++ stories/index.js
@@ -23,6 +23,8 @@ import Empty from   "components/Appointment/Empty.js";
 
 import Show from   "components/Appointment/Show.js";
 
+import Confirm from "components/Appointment/Confirm.js";
+
 storiesOf("Button", module)
   .addParameters({
     backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
@@ -159,4 +161,10 @@ storiesOf("Appointment", module)
       interviewer={interviewers}
       onEdit={action("onEdit")}
       onDelete={action("onDelete")}
+   />)
+   .add("Confirm", () => <Confirm
+      message={"Delete the appointment?"}
+      onConfirm={action("onConfirm")}
+      onCancel={action("onCancel")}
+
    />)
