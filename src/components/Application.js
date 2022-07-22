8f4b6d79ccb7e65f59323a39caaed1d2aec6c4dc
siopao2001 <sio_pao2001@yahoo.ca>
2022-07-22 11:50:37 -0400

    add UseEffect and axios to request days from api

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,4 +1,6 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
+
+import axios from "axios"
 
 import "components/Application.scss";
 
@@ -6,23 +8,7 @@ import DayList from "./DayList";
 
 import Appointment from "./Appointment/index";
 
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
+
 
 const appointments = {
   "1": {
@@ -69,12 +55,20 @@ const appointments = {
 
 export default function Application(props) {
   const [day, setDay] = useState("Monday");
+  const [days, setDays] = useState([]);
   const AppointmentListData = Object.values(appointments).map(appointment => {
       return(<Appointment
        key={appointment.id}
        {...appointment}
      />)
-   }) 
+   })
+  useEffect(() => {
+      const daysAPI = "http://localhost:8001/api/days";
+      axios.get(daysAPI).then((response) => {
+       console.log(response.data)
+       setDays([...response.data]);
+   })
+  }, [])
 
   return (
     <main className="layout">
