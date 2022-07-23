427b7b29cde83887d4b34ebe6e90d3caf11dd754
siopao2001 <sio_pao2001@yahoo.ca>
2022-07-23 10:07:56 -0400

    modify state to be in one object

--- src/components/Application.js
+++ src/components/Application.js
@@ -9,7 +9,6 @@ import DayList from "./DayList";
 import Appointment from "./Appointment/index";
 
 
-
 const appointments = {
   "1": {
     id: 1,
@@ -54,8 +53,12 @@ const appointments = {
 };
 
 export default function Application(props) {
-  const [day, setDay] = useState("Monday");
-  const [days, setDays] = useState([]);
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+  })
+  const setDay = day => setState({ ...state, day })
+  const setDays = days => setState(prev => ({ ...prev, days }))
   const AppointmentListData = Object.values(appointments).map(appointment => {
       return(<Appointment
        key={appointment.id}
@@ -81,8 +84,8 @@ export default function Application(props) {
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
           <DayList
-           days={days}
-           value={day}
+           days={state.days}
+           value={state.day}
            onChange={setDay}
           />
         </nav>
