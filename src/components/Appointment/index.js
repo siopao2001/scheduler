1e4129a5f0a9d947899d48e7abe274d506d7e7c6
siopao2001 <sio_pao2001@yahoo.ca>
2022-08-05 13:59:02 -0400

    Implement integration testing to check if appointment are loeaded, interviews can be booked and spots updated

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -48,7 +48,7 @@ function remove() {
 };
  
    return(
-     <article className="appointment">
+     <article data-testid="appointment" className="appointment">
       <Header time={props.time}/>
        {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
        {mode === SHOW && <Show student={props.interview.student} interviewer={props.interview.interviewer} onEdit={()=>transition(EDIT)} onDelete={()=>transition(CONFIRM)}/> }
